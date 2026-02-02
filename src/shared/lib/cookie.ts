0c37d77e51e44f3128c6bba32847f5a3ad248e09
hell-soon <pmaks2365@gmail.com>
2026-02-02 17:23:54 +0300

    up package

--- src/shared/lib/cookie.ts
+++ src/shared/lib/cookie.ts
@@ -21,14 +21,12 @@ export function setCookie(name: string, value: string, days: number): void {
  * @returns Значение cookie или null, если cookie не найдена.
  */
 export function getCookie(name: string): string | null {
-  const nameEQ = `${name}=`
-  const ca = document.cookie.split(';')
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i]
-    while (c.charAt(0) === ' ')
-      c = c.substring(1, c.length)
-    if (c.indexOf(nameEQ) === 0)
-      return c.substring(nameEQ.length, c.length)
+  const value = `; ${document.cookie}`
+  const parts = value.split(`; ${name}=`)
+
+  if (parts.length === 2) {
+    return parts.pop()?.split(';').shift() || null
   }
+
   return null
 }
