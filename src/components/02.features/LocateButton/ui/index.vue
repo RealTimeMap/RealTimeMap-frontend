e2f7167a1eab7e7a558c265b21b3752852f20a15
hell-soon <pmaks2365@gmail.com>
2026-04-27 11:44:47 +0300

    fix pos button locate

--- src/components/02.features/LocateButton/ui/index.vue
+++ src/components/02.features/LocateButton/ui/index.vue
@@ -18,41 +18,45 @@ function centerOnUser() {
 </script>
 
 <template>
-  <button
+  <u-glass-wrapper
     v-if="userPosition"
     class="locate-button"
-    @click="centerOnUser"
+    :scale="40"
   >
-    <u-icon
-      icon="line-md:my-location"
-      width="20"
-      height="20"
-    />
-  </button>
+    <button
+      @click="centerOnUser"
+    >
+      <u-icon
+        icon="line-md:my-location"
+        width="20"
+        height="20"
+      />
+    </button>
+  </u-glass-wrapper>
 </template>
 
 <style lang="scss" scoped>
 .locate-button {
   position: absolute;
   right: 20px;
-  bottom: 120px;
-
-  border-radius: 20px;
-  overflow: hidden;
+  bottom: 160px;
   backdrop-filter: blur(38.4px) saturate(180%);
-  background: rgba(18, 24, 38, 0.45);
-  border: 0.5px solid rgba(255, 255, 255, 0.12);
-  box-shadow:
-    rgba(255, 255, 255, 0.06) 0px 1px 0px inset,
-    rgba(0, 0, 0, 0.35) 0px 10px 30px;
   width: 44px;
   height: 44px;
-  display: flex;
-  align-items: center;
-  justify-content: center;
+  border-radius: 20px;
 
-  svg {
-    color: white;
+  button {
+    width: 100%;
+    height: 100%;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    border: none;
+    background: transparent;
+    border-radius: 20px;
+    svg {
+      color: white;
+    }
   }
 }
 </style>
