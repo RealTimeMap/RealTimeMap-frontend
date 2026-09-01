6cf34a4de14f9783ebf01eca97253ced99e6e51e
hell-soon <pmaks2365@gmail.com>
2026-09-01 12:15:04 +0300

    style(ui): Мелкие правки цветов

--- src/components/01.kit/UFlag/ui/index.vue
+++ src/components/01.kit/UFlag/ui/index.vue
@@ -22,7 +22,7 @@ const statusClass = computed(() => props.status ? `u-flag--${props.status}` : ''
   border-radius: 6px;
 
   &--Active {
-    color: oklch(0.85 0.18 150);
+    color: var(--access-color);
     background: oklch(0.4 0.12 150 / 0.25);
     border: 0.5px solid oklch(0.65 0.18 150 / 0.4);
   }
